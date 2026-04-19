bc96994891a08bd506404f33c524b31bb9f6a227
Lohann <lohacharubi@gmail.com>
2026-04-19 12:09:29 -0400

    dashboard navigation done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,28 @@
 import { Session } from "../util/Session";
+import { useNavigate } from "react-router-dom"
 
 function Header({ user, headerTitle }) {
+
+    var additionalNavButtons = ""
+    if (user.admin) {
+        additionalNavButtons = <div>
+            <button onClick={() => navigate("../dashboard")}>Go to Player Dashboard</button>
+            <button onClick={() => navigate("../admin_dashboard")}>Go to Admin Dashboard</button>
+        </div>
+    }
+
+
+    const navigate = useNavigate()
+
     return (
         <header className="dashboard-header">
             {/* <div className="logo">LOGO</div> */}
             <nav>
-                <span>Welcome {user.lname}, {user.fname}</span>
-                <button className="button btn-logout" onClick={() => Session.logout()}>Logout</button>
+                <div>
+                    <span>Welcome {user.lname}, {user.fname}</span>
+                    <button className="button btn-logout" onClick={() => Session.logout()}>Logout</button>
+                </div>
+                {additionalNavButtons}
             </nav>
             <h1>{headerTitle}</h1>
         </header>
